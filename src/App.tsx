6b789705a789b4f42f47432145f62766ab2861ba
Carlos Nayan <carlosnayan@gmail.com>
2024-08-12 20:11:08 -0300

    inicial setup of application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { Player } from "./pages/Player";
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <Provider store={store}>
+      <Player />
+    </Provider>
+  );
+}
